63a145b41218e5db9ff537037d2faa5953114f1d
Bediako George <bediako.george@georgetownsoftware.house>
2024-09-04 10:17:49 -0400

    Changed POM

--- build.clj
+++ build.clj
@@ -1,7 +1,7 @@
 (ns build
   (:require [clojure.tools.build.api :as b]))
 
-(def lib 'georgetown-software-house/clojure.cache)
+(def lib 'georgetown-software-house/georgetown-software-house.cache)
 (def version (format "1.2.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
